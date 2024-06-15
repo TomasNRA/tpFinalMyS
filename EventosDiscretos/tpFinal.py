462df24d499f712b2e7425dcdb363b7571dba405
TomasNRA <tom007_ra@hotmail.com>
2024-06-14 22:45:29 -0300

    Cambios con nuevo enunciado

--- EventosDiscretos/tpFinal.py
+++ EventosDiscretos/tpFinal.py
@@ -8,70 +8,73 @@ import numpy as np
 stock_auto_parte1 = 0
 stock_auto_parte2 = 0
 demanda = 0
+unidades_totales = 0
 
 class Deposito(object):
     def __init__(self, env, num_operarios):
         self.env = env
         self.operario = simpy.Resource(env, num_operarios)
-        self.se_desocupa_operario = simpy.Store(env)
-        self.ahora_no = env.event()
+        self.no_hay_stock = env.event()
 
     def atender_requerimiento(self):
         global stock_auto_parte1
         global stock_auto_parte2
         global demanda
-        if (stock_auto_parte1 > 0) & (stock_auto_parte2 > 0):
+        if (stock_auto_parte1 > 0) and (stock_auto_parte2 > 0):
             stock_auto_parte1 -= 1
             stock_auto_parte2 -= 1
             demanda -= 1
-            #aca van los tiempos exponenciales
-            yield self.env.timeout(15)
+            yield self.env.timeout(np.random.exponential(scale=8)+np.random.exponential(scale=12))
         else:
-            yield self.ahora_no
+            print('me quede sin stock')
+            yield self.no_hay_stock
 
 def solicitar_autopartes(env,deposito):
+    global unidades_totales
     with deposito.operario.request() as request:
         yield request
-        """ print(f'{deposito.operario.count} of {deposito.operario.capacity} slots are allocated.') """
         yield env.process(deposito.atender_requerimiento())
-        req = yield deposito.se_desocupa_operario.get()
-        req.succeed()
+        unidades_totales += 1
 
 def run_day(env,deposito):
     global demanda
-    demandaDiaria = np.random.poisson(lam=56)
-    demanda += demandaDiaria
-    print(f'LLego demanda {demandaDiaria} y la total es {demanda}')
-    for algo in range(deposito.operario.capacity):
-        env.process(solicitar_autopartes(env,deposito))
-    while demanda>1:
-            req = env.event()
-            deposito.se_desocupa_operario.put(req)
-            yield req
+    print(f'Al comenzar el dia, la demanda es: {demanda}')
+
+    #Si arranca el dia con demanda insatisfecha, se etiende 
+    if demanda > 0:
+        for algo in range(demanda):
             env.process(solicitar_autopartes(env,deposito))
 
+    #Se va generando demanda con un tiempo entre llegadas con una distribucion exponencial
+    while True:
+        demanda +=1
+        env.process(solicitar_autopartes(env,deposito))
+        yield env.timeout(np.random.exponential(scale=1/0.0972)) #0.0972 sale de dividir 70 por la jornada laboral
+
 def fabricarAutoPartes(stock_auto_parte1,stock_auto_parte2):
-    return stock_auto_parte1+120, stock_auto_parte2+150          
+    return stock_auto_parte1+152, stock_auto_parte2+152
 
 def main():
     horas_max = 12
     jornada_laboral = horas_max*60
-    num_operarios_deposito = 2
+    num_operarios_deposito = 3
     global stock_auto_parte1
     global stock_auto_parte2
     global demanda
-  # Run the simulation
+    global unidades_totales
+
     for anio in range(3):
             for mes in range(12):
                 for dia in range(30):
                     env = simpy.Environment()
                     deposito = Deposito(env, num_operarios_deposito)
-                    if dia <= 15:
+                    if dia <= 13:
                         stock_auto_parte1, stock_auto_parte2 = fabricarAutoPartes(stock_auto_parte1,stock_auto_parte2)
                     print(f'dia {dia}/{mes}/{anio} el stock es {stock_auto_parte1,stock_auto_parte2}')
                     env.process(run_day(env,deposito))
                     env.run(until=jornada_laboral)
                     print(f'La demanda quedo en {demanda}')
+    print(f'{unidades_totales=}')
 
 if __name__ == '__main__':
     main()
